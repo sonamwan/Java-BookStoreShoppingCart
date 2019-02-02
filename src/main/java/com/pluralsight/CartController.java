0ac6744752c68c7c3aedb1afbe5ea7ae2ce9f07c
Sonam Wangchuk <sonamwan@buffalo.edu>
2019-02-01 19:31:23 -0500

    Deleting from Shopping cart

--- src/main/java/com/pluralsight/CartController.java
+++ src/main/java/com/pluralsight/CartController.java
@@ -44,6 +44,8 @@ public class CartController extends HttpServlet {
 			switch(action) {
 				case "/addcart":
 					 addToCart(request, response);
+				case "/delete":
+					 deleteFromCart(request,response);
            break;
         default:
            break;
@@ -54,6 +56,14 @@ public class CartController extends HttpServlet {
 		}
 
 		response.sendRedirect("../ShoppingCart.jsp");
+	}
+
+	private void deleteFromCart(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		int index = Integer.parseInt(request.getParameter("index"));
+		ShoppingCart cart = (ShoppingCart)session.getAttribute("cart");
+		cart.deleteCartItem(index);
 	}
 
   protected void addToCart(HttpServletRequest request, HttpServletResponse response)
